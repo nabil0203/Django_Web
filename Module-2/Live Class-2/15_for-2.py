19542131422844858a302adabca76e8910590b48
Chowdhury Nabil Ahmed <115895668+nabil0203@users.noreply.github.com>
2025-05-17 14:14:12 +0600

    update

--- Module-2/Live Class-2/15_for-2.py
+++ Module-2/Live Class-2/15_for-2.py
@@ -1,4 +1,4 @@
-for i in range(10):  # range means, it creates a Iterable--> [0, 1, 2, 3, 4]
+for i in range(10):                    # range means, it creates an Iterable--> [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     print(i)
 
 
@@ -7,7 +7,7 @@ print("--------")
 
 
 
-for i in range(3, 10):
+for i in range(3, 10):                # 3 to 9
     print(i)
 
 
@@ -16,5 +16,5 @@ print("--------")
 
 
 
-for i in range(4, 20, 3):
+for i in range(4, 20, 3):            # 4 to 20; gap of 3 
     print(i)
