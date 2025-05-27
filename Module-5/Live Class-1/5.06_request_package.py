e3e913264605d234818d913ab0730237b9175880
Chowdhury Nabil Ahmed <nabilahmed0203@gmail.com>
2025-05-27 18:17:41 +0600

    update

--- Module-5/Live Class-1/5.06_request_package.py
+++ Module-5/Live Class-1/5.06_request_package.py
@@ -1,3 +1,8 @@
+# check Pre-recorded --> 5.10
+# to ger e clear concept
+
+
+
 import requests
 
 response = requests.get("https://api.github.com")               # get() - method is used to hit/extract data from a url
