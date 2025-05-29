8b10624a68779668b8986651f78e00e6379f9b3a
Chowdhury Nabil Ahmed <115895668+nabil0203@users.noreply.github.com>
2025-05-29 18:05:29 +0600

    update

--- Module-5/Live Class-1/5.02_module_import_shortcut/01_my_module.py
+++ Module-5/Live Class-1/5.02_module_import_shortcut/01_my_module.py
@@ -4,18 +4,18 @@
 # import greet                                  # not importing the whole module
 
 
-from greet import greetings                     # just importing a specific method of the module
+from greet import greetings                     # (1)-->just importing a specific method of the module
 # greet.greetings("Nabillllll")                 # no need to write the module name
 greetings("Nabillllll")
 
 
 
-from car import start as st                     # importing a specific method as a different name
+from car import start as st                     # (2)-->importing a specific method as a different name
 st()                                            # method called of the module
 
 
 
 
-import person as pr                             # even we can import the module with a differnt name
+import person as pr                             # (3)-->even we can import the module with a differnt name
 pr.habit()
 
