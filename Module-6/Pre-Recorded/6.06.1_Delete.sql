db792f8df31d2929278226928b19ccbfc146c10e
Chowdhury Nabil Ahmed <115895668+nabil0203@users.noreply.github.com>
2025-06-01 00:30:30 +0600

    rename

--- Module-6/Pre-Recorded/6.06.1_Delete.sql
+++
@@ -1,5 +0,0 @@
-
--- Delete item
--- Products that has lower stock than 10
-
-DELETE FROM products WHERE stock < 10
---
+++ Module-6/Pre-Recorded/6.06._Delete.sql
@@ -0,0 +1,5 @@
+
+-- Delete item
+-- Products that have a lower stock than 10
+
+DELETE FROM products WHERE stock < 10
