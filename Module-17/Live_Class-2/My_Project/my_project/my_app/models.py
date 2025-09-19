6e16716003bca9cc27aec55cb2d3e4fcef2a920b
Chowdhury Nabil Ahmed <nabilahmed0203@gmail.com>
2025-09-19 12:37:09 +0600

    mod-17 live-2

--- Module-17/Live_Class-2/My_Project/my_project/my_app/models.py
+++ Module-17/Live_Class-2/My_Project/my_project/my_app/models.py
@@ -5,3 +5,7 @@ from django.db import models
 class Blog(models.Model):                            # Blog = Table name of SQL
     name = models.CharField(max_length=100)          # "name" is a Column
     content = models.TextField()                     # "content" is a column
+
+
+    def __str__(self):                               # this shows the actual blogs that we have created --> First Blog, Second Blog
+        return self.name                             # without it --> Blog object (1), Blog object (2)
--- Module-17/Live_Class-2/My_Project/my_project/my_app/views.py
+++ Module-17/Live_Class-2/My_Project/my_project/my_app/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse                    # imported
-
+from . models import Blog                               # imported
 
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
 
+# def about(request):
+#     return HttpResponse("This is About Page.")
+
+
+
 def about(request):
-    return HttpResponse("This is About Page.")
+
+    # fetch all the contents
+    blogs = Blog.objects.all()                                            # similar sql --> # select * from blog
+
+    return render(request, 'about.html', {'blogs' : blogs})               # render(request, template file, context)
