3099d018f36d387db6a56d1f40c264da42d2985d
Chowdhury Nabil Ahmed <nabilahmed0203@gmail.com>
2025-09-27 12:19:23 +0600

    mod-18_Live-1

--- Module-18/Live_Class-1/My_project/student_management/student_app/urls.py
+++ Module-18/Live_Class-1/My_project/student_management/student_app/urls.py
@@ -8,5 +8,16 @@ urlpatterns = [
 
     path('', views.list_students, name='list_students'),
     path('create/', views.create_students, name='create_students'),
-    
+    path('edit/<int:id>', views.update_students, name='update_students'),
+    path('delete/<int:id>', views.delete_students, name='delete_students')
 ]
+
+
+
+
+
+
+# path('', ..........)----->    here, 'blank' means if we visit "127.0.0.1" then the "list_student"-function of view.py will execute. And that is designed to run student_list.html
+# path('create/', ..........)----->     here, 'create/' means if we visit "127.0.0.1/create" then the "create_student"-function of view.py will execute. And that is designed to run student_create.html
+# path('edit/<int:id>', ..........)----->    here, 'edit/<int:id>' means if we click on any 'edit' button, then the "update_student"-function of view.py will execute. It will take to that specific object's page
+
--- Module-18/Live_Class-1/My_project/student_management/student_app/views.py
+++ Module-18/Live_Class-1/My_project/student_management/student_app/views.py
@@ -1,14 +1,79 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
-from . import models
+from . import models, forms
+
+
 
 # Create your views here.
 
+
 def list_students(request):
-    students = models.Student.objects.all()
-    return render(request, 'student_list.html', {'students' : students})
+    student_app = models.Student.objects.all()
+    return render(request, "student_list.html", {"students": student_app})
+
+
+# --------------------------------------------------------------------------------------------------
 
 
 def create_students(request):
-    return render(request, 'student_create.html')
 
+# HTML Form
+    # if request.method == 'POST':
+    #     name = request.POST.get('name')
+    #     email = request.POST.get('email')
+    #     age = request.POST.get('age')
+
+    #     models.Student.objects.create(
+    #         name = name,
+    #         email = email,
+    #         age = age
+    #     )
+
+    #     return redirect(list_students)
+
+
+
+# Model Form
+    if request.method == "POST":
+        form = forms.StudentForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect(list_students)
+
+    form = forms.StudentForm()
+    return render(request, 'student_create.html', {'form' : form})
+
+
+
+
+# --------------------------------------------------------------------------------------------------
+
+
+def update_students(request, id):
+
+    student_app = get_object_or_404(models.Student, id=id)
+    
+    if request.method == "POST":
+        form = forms.StudentForm(request.POST, instance=student_app)
+
+        if form.is_valid():
+            form.save()
+            return redirect(list_students)
+
+    
+    form = forms.StudentForm(request.POST or None, instance=student_app)
+    return render(request, 'student_create.html', {'form' : form})
+    
+
+
+# --------------------------------------------------------------------------------------------------
+
+
+def delete_students(request, id):
+
+    student_app = get_object_or_404(models.Student, id=id)
+    
+    student_app.delete()
+    return redirect('list_students')
+    
