d02ddf07fef97523c635b3fa24e10db18373f320
Chowdhury Nabil Ahmed <nabilahmed0203@gmail.com>
2025-10-05 19:21:57 +0600

    update all

--- Module-19/Live_Class-1/My_project/student_management/student_app/views.py
+++ Module-19/Live_Class-1/My_project/student_management/student_app/views.py
@@ -4,7 +4,8 @@ from . import models, forms
 
 from django.contrib import messages
 
-from django.views.generic import ListView
+from django.views.generic import ListView,CreateView,UpdateView,DeleteView
+from django.urls import reverse_lazy
 
 
 
@@ -14,26 +15,32 @@ from django.views.generic import ListView
 
 
 # -----------------Function based view--------------
-def list_students(request):
-    student_app = models.Student.objects.all()
-    return render(request, "student_list.html", {"students": student_app})
+# def list_students(request):
+#     student_app = models.Student.objects.all()
+#     return render(request, "student_list.html", {"students": student_app})
 
 
 
-# --------------------Class based view-------------
-# class StudentViewList(ListView):
-#     template_name = 'student_list.html'
-#     model = models.Student
-#     context_object_name = 'students'
+# --------------------Class based view-----------------
+class StudentViewList(ListView):
+    template_name = 'student_list.html'
+    model = models.Student
+    context_object_name = 'students'
 
 
 
 # --------------------------------------------------------------------------------------------------
 
 
-def create_students(request):
 
-# HTML Form
+
+
+
+# -----------------Function based view-------------------
+
+
+# ----------HTML Form--------------
+# def create_students(request):
     # if request.method == 'POST':
     #     name = request.POST.get('name')
     #     email = request.POST.get('email')
@@ -49,17 +56,36 @@ def create_students(request):
 
 
 
-# Model Form
-    if request.method == "POST":
-        form = forms.StudentForm(request.POST)
+# --------------Model Form--------------
+# def create_students(request):
+    # if request.method == "POST":
+    #     form = forms.StudentForm(request.POST)
 
-        if form.is_valid():
-            form.save()
-            messages.success(request, "New Student Created Successfully")
-            return redirect(list_students)
+    #     if form.is_valid():
+    #         form.save()
+    #         messages.success(request, "New Student Created Successfully")
+    #         return redirect(list_students)
 
-    form = forms.StudentForm()
-    return render(request, 'student_create.html', {'form' : form})
+    # form = forms.StudentForm()
+    # return render(request, 'student_create.html', {'form' : form})
+
+
+
+
+
+# --------------------Class based view-----------------
+
+class StudentCreateView(CreateView):
+    model = models.Student
+    form_class = forms.StudentForm
+    template_name = 'student_create.html'
+    success_url = reverse_lazy('list_students')
+    
+    def form_valid(self, form):
+        """If the form is valid, save the associated model."""
+        messages.success(self.request,'Student added successfully!')
+        return super().form_valid(form)
+
 
 
 
@@ -67,32 +93,71 @@ def create_students(request):
 # --------------------------------------------------------------------------------------------------
 
 
-def update_students(request, id):
 
-    student_app = get_object_or_404(models.Student, id=id)
-    
-    if request.method == "POST":
-        form = forms.StudentForm(request.POST, instance=student_app)
 
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Student's Information updated Successfully")
-            return redirect(list_students)
+# -----------------Function based view-------------------
+
+# def update_students(request, id):
+
+#     student_app = get_object_or_404(models.Student, id=id)
+    
+#     if request.method == "POST":
+#         form = forms.StudentForm(request.POST, instance=student_app)
+
+#         if form.is_valid():
+#             form.save()
+#             messages.success(request, "Student's Information updated Successfully")
+#             return redirect(list_students)
 
     
-    form = forms.StudentForm(request.POST or None, instance=student_app)
-    return render(request, 'student_create.html', {'form' : form, 'isUpdated' : True})
+#     form = forms.StudentForm(request.POST or None, instance=student_app)
+#     return render(request, 'student_create.html', {'form' : form, 'isUpdated' : True})
+
+
+
+
+# --------------------Class based view-----------------
+class StudentUpdateView(UpdateView):
+    model = models.Student
+    form_class = forms.StudentForm
+    template_name = 'student_create.html'
+    success_url = reverse_lazy('list_students')
     
+    def form_valid(self, form):
+        """If the form is valid, save the associated model."""
+        messages.success(self.request,'Student updated successfully!')
+        return super().form_valid(form)
+
+
+
+
 
 
 # --------------------------------------------------------------------------------------------------
 
 
-def delete_students(request, id):
 
-    student_app = get_object_or_404(models.Student, id=id)
+
+# -----------------Function based view-------------------
+
+# def delete_students(request, id):
+#     student_app = get_object_or_404(models.Student, id=id)
     
-    student_app.delete()
-    messages.success(request, "Student Deleted Successfully")
-    return redirect('list_students')
+#     student_app.delete()
+#     messages.success(request, "Student Deleted Successfully")
+#     return redirect('list_students')
+
+
+
+
+# --------------------Class based view-----------------
+class StudentDeleteView(DeleteView):
+    model = models. Student
+    template_name ='student_delete.html'
+    success_url = reverse_lazy('list_students')
+    pk_url_kwarg = 'id'
+
+    def delete(self, request, *args, ** kwargs):
+        messages.success(self.request, 'Student deleted Successfully')
+        return super().delete(request, *args, ** kwargs)
     
